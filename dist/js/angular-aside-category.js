4ee8fa3833ea9a220de5e8c7cab8909fdaf890fd
afei <88603982@qq.com>
2016-05-21 18:32:35 +0800

    update gulp script

---
+++ dist/js/angular-aside-category.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular.module('nx.widget', []);
+
+})();
+
+(function () {
+  'use strict';
+
+  angular.module('nx.widget')
+    .directive('nxAsideCategory', [function ($scope) {
+      return {
+        restrict: 'E',
+        scope: {
+          cssClass: '@',
+          items: '='
+        },
+        template: '<div class="ng-widget-tabs {{cssClass}}">' +
+        '<ul class="tab-hd">' +
+        '<li class="tab-hd-item" ng-repeat="item in items" data-active="{{item.selected}}">' +
+        '<a href="javascript:;" ng-click="select(item)">{{item.title}}</a>' +
+        '</li>' +
+        '</ul>' +
+        '<div class="tab-bd" ng-transclude></div>' +
+        '</div>'
+      };
+
+    }]);
+
+
+})();
